69ffbb15ef91bad29093e9e6b1b3207a163bda87
Matthijs Kralt <matthijs29@live.com>
2020-03-28 10:14:31 +0100

    Add setchannel action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 export const SET_MESSAGES = 'SET_MESSAGES';
+export const SET_CHANNELS = 'SET_CHANNELS';
 const API = 'http://localhost:3000';
 
 export function setMessages() {
@@ -6,9 +7,22 @@ export function setMessages() {
   return fetch(`${API}/messages`)
     .then(res => res.json())
     .then(data => {
-      
+
       return {
         type: SET_MESSAGES,
+        payload: data
+      }
+    });
+}
+
+export function setChannels() {
+  // fetch messages from db
+  return fetch(`${API}/channels`)
+    .then(res => res.json())
+    .then(data => {
+      
+      return {
+        type: SET_CHANNELS,
         payload: data
       }
     });
