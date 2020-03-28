4a8375192710b8e62f4af988f05d2cda58c3e788
Matthijs Kralt <matthijs29@live.com>
2020-03-28 11:19:13 +0100

    Connect sendmessage component with redux

--- src/containers/SendMessage.js
+++ src/containers/SendMessage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { sendMessage } from '../actions';
+
 const StyledInput = styled(Input)`
   background-color: #3D3B3C;
   color: #ededed;
@@ -14,23 +18,41 @@ const StyledInput = styled(Input)`
 
 class SendMessage extends React.Component {
   state = {
-    message: 'Hello'
+    message: ''
+  }
+
+  handleChange = name => e => {
+    this.setState({ [name]: e.target.value });
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log('submit');
+    const { message } = this.state;
+    const { sendMessage, currentUser } = this.props;
+    if (message) {
+      sendMessage(message, currentUser.name);
+    }
   }
   render () {
     const { message } = this.state;
     return (
       <Form onSubmit={this.handleSubmit}>
         <FormGroup>
-          <StyledInput type="text" value={message} />
+          <StyledInput type="text" value={message} onChange={this.handleChange('message')} />
         </FormGroup>
       </Form>
     )
   }
 }
 
-export default SendMessage;
+function mapStateToProps(state) {
+  return { 
+    currentUser: state.currentUser
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ sendMessage }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SendMessage);
